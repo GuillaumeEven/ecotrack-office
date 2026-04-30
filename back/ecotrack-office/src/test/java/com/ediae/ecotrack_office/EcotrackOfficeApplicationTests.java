7d0a4010f0e2450ec896ed3b58ad1d97ebccca69
GuillaumeEven <guillaume@evendev.net>
2026-04-30 17:08:44 +0200

    chore(): install spring

---
+++ back/ecotrack-office/src/test/java/com/ediae/ecotrack_office/EcotrackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ediae.ecotrack_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcotrackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
