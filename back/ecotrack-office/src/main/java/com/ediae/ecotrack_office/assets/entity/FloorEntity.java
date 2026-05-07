5dda870588f9bb83986d3cc3aaa906b00437a4cb
GuillaumeEven <guillaume@evendev.net>
2026-05-07 18:55:13 +0200

    fix(): merge organization relation in floorEntity

--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/assets/entity/FloorEntity.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/assets/entity/FloorEntity.java
@@ -1,27 +1,15 @@
 package com.ediae.ecotrack_office.assets.entity;
 
-import java.util.List;
-
-<<<<<<< Updated upstream
-=======
-import java.util.List;
-
 import com.ediae.ecotrack_office.organization.entity.OrganizationEntity;
->>>>>>> Stashed changes
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-<<<<<<< Updated upstream
-=======
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
->>>>>>> Stashed changes
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import com.ediae.ecotrack_office.organization.entity.OrganizationEntity;
 
 @Entity
 @Table(name = "ast_floors")
@@ -37,4 +25,48 @@ public class FloorEntity {
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = false)
+    private OrganizationEntity organization;
+
+    public FloorEntity() {
+    }
+
+    public FloorEntity(Integer level, Boolean isActive, OrganizationEntity organization) {
+        this.level = level;
+        this.isActive = isActive;
+        this.organization = organization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public OrganizationEntity getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(OrganizationEntity organization) {
+        this.organization = organization;
+    }
 }
