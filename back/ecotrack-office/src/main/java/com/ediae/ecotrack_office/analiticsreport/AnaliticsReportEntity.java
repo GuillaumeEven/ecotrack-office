cab9e03147c85bf114e015a660e9e4c9ed67acce
GuillaumeEven <guillaume@evendev.net>
2026-05-07 19:13:22 +0200

    fix(): fix broken imports

--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/analiticsreport/AnaliticsReportEntity.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/analiticsreport/AnaliticsReportEntity.java
@@ -2,7 +2,16 @@ package com.ediae.ecotrack_office.analiticsreport;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import com.ediae.ecotrack_office.organization.entity.OrganizationEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "analitics_report")
--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/incident/model/IncidentEntity.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/incident/model/IncidentEntity.java
@@ -2,10 +2,20 @@ package com.ediae.ecotrack_office.incident.model;
 
 import java.time.LocalDateTime;
 
+import com.ediae.ecotrack_office.assets.entity.ResourceEntity;
 import com.ediae.ecotrack_office.incident.enums.IncidentStatus;
+import com.ediae.ecotrack_office.users.entity.UserEntity;
 
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.CriteriaBuilder.In;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table (name = "incidents")
