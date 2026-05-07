db8387ef56c32e8ed5d52f985fa7b1a83457b9b2
joseluis121200 <joseluisrrr9@gmail.com>
2026-05-07 12:18:55 +0200

    Refactor User model to include relationships
    Updated organizationId to use ManyToOne relationship and added incidences and reservations lists.

--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/users/model/User.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/users/model/User.java
@@ -27,8 +27,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
-    @Column(name = "organization_id", nullable = false)
+    
+    @ManyToOne (optional = false)
+    @JoinColumn (name = "organization_id", nullable = false)
     private Long organizationId;
 
     @Column(name = "is_active", nullable = false)
@@ -41,6 +42,14 @@ public class User {
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @OneToMany (mapped by = "userId")
+    Private List<IncidenceEntity> incidences;
+
+    @OneToMany (mapped by = "userId")
+    Private List<ReservationEntity> reservations;
+
+    
 
     // Getters and Setters
     public Long getId() { return id; }
