a758ebbb0038a7140ddc9cbe4e52f78b30983eeb
GuillaumeEven <guillaume@evendev.net>
2026-05-19 23:53:12 +0200

    refactor: update FloorService and FloorController to use FloorModel instead of FloorResponseDto

--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/assets/service/FloorService.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/assets/service/FloorService.java
@@ -1,16 +1,16 @@
 package com.ediae.ecotrack_office.assets.service;
 
 import com.ediae.ecotrack_office.assets.dto.FloorRequestDto;
-import com.ediae.ecotrack_office.assets.dto.FloorResponseDto;
+import com.ediae.ecotrack_office.assets.model.FloorModel;
 
 
 public interface FloorService {
 
-    public FloorResponseDto getFloorById(Long id);
+    public FloorModel getFloorById(Long id);
 
-    public FloorResponseDto createFloor(FloorRequestDto floorRequestDto);
+    public FloorModel createFloor(FloorRequestDto floorRequestDto);
 
-    public FloorResponseDto updateFloor(Long id, FloorRequestDto floorRequestDto);
+    public FloorModel updateFloor(Long id, FloorRequestDto floorRequestDto);
 
     public void deleteFloor(Long id);
 
