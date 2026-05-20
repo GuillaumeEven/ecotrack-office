507aaf2366c57b13b0217daa2d30933ff8986ff0
RaiMorales <raimundo.niwe@gmail.com>
2026-05-20 09:18:09 +0200

    feat(): añado mapper a Organization y elimino los métodos ya duplicados en OrganizationModel

---
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/organization/mapper/OrganizationMapper.java
@@ -0,0 +1,94 @@
+package com.ediae.ecotrack_office.organization.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.ediae.ecotrack_office.organization.dto.OrganizationCreateDto;
+import com.ediae.ecotrack_office.organization.dto.OrganizationResponseDto;
+import com.ediae.ecotrack_office.organization.dto.OrganizationUpdateDto;
+import com.ediae.ecotrack_office.organization.entity.OrganizationEntity;
+import com.ediae.ecotrack_office.organization.model.OrganizationModel;
+
+@Component
+public class OrganizationMapper {
+
+    // Médoto para pasar de OrganizationEntity a OrganizationModel
+
+    public static OrganizationModel fromEntity (OrganizationEntity entity) {
+
+        return new OrganizationModel(
+            entity.getId(),
+            entity.getName(),
+            entity.getCIF(),
+            entity.getAddress(),
+            entity.getEmail(),
+            entity.getEndSubscription(),
+            entity.getIsActive(),
+            entity.getCreatedAt()
+        );
+    }
+
+    // Método para pasar de OrganizationModel a OrganizationEntity
+
+    public OrganizationEntity toEntity (OrganizationModel model) {
+        
+        OrganizationEntity entity = new OrganizationEntity();
+        entity.setId(model.getId());
+        entity.setName(model.getName());
+        entity.setCIF(model.getCIF());
+        entity.setAddress(model.getAddress());
+        entity.setEmail(model.getEmail());
+        entity.setEndSubscription(model.getEndSubscription());
+        entity.setIsActive(model.getIsActive());
+        entity.setCreatedAt(model.getCreatedAt());
+        return entity;
+    }
+
+    // Método para pasar de OrganizationCreateDto a OrganizationModel
+
+    public static OrganizationModel fromCreateDto (OrganizationCreateDto createDto) {
+
+        return new OrganizationModel (
+            null,
+            createDto.getName(),
+            createDto.getCIF(),
+            createDto.getAddress(),
+            createDto.getEmail(),
+            createDto.getEndSubscription(),
+            createDto.getIsActive(),
+            createDto.getCreatedAt()
+         );
+    }
+
+    // Método para pasar de un OrganizationUpdateDto a OrganizationModel
+
+    public static OrganizationModel fromUpdateDto (OrganizationUpdateDto updateDto) {
+
+        return new OrganizationModel (
+            updateDto.getId(),
+            updateDto.getName(),
+            updateDto.getCIF(),
+            updateDto.getAddress(),
+            updateDto.getEmail(),
+            updateDto.getEndSubscription(),
+            updateDto.getIsActive(),
+            updateDto.getCreatedAt()
+         );
+    }
+
+    // Método para pasar de OrganizationModel a OrganizationResponseDto
+
+    public OrganizationResponseDto toResponseDto (OrganizationModel model) {
+
+        OrganizationResponseDto responseDto = new OrganizationResponseDto();
+        responseDto.setId(model.getId());
+        responseDto.setName(model.getName());
+        responseDto.setCIF(model.getCIF());
+        responseDto.setAddress(model.getAddress());
+        responseDto.setEmail(model.getEmail());
+        responseDto.setEndSubscription(model.getEndSubscription());
+        responseDto.setIsActive(model.getIsActive());
+        responseDto.setCreatedAt(model.getCreatedAt());
+        return responseDto;
+    }
+
+}
--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/organization/model/OrganizationModel.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/organization/model/OrganizationModel.java
@@ -3,11 +3,6 @@ package com.ediae.ecotrack_office.organization.model;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-import com.ediae.ecotrack_office.organization.dto.OrganizationCreateDto;
-import com.ediae.ecotrack_office.organization.dto.OrganizationResponseDto;
-import com.ediae.ecotrack_office.organization.dto.OrganizationUpdateDto;
-import com.ediae.ecotrack_office.organization.entity.OrganizationEntity;
-
 public class OrganizationModel {
 
     // Atributos
@@ -94,82 +89,4 @@ public class OrganizationModel {
         this.createdAt = createdAt;
     }
 
-    // Médoto para pasar de OrganizationEntity a OrganizationModel
-
-    public static OrganizationModel fromEntity (OrganizationEntity entity) {
-
-        return new OrganizationModel(
-            entity.getId(),
-            entity.getName(),
-            entity.getCIF(),
-            entity.getAddress(),
-            entity.getEmail(),
-            entity.getEndSubscription(),
-            entity.getIsActive(),
-            entity.getCreatedAt()
-        );
-    }
-
-    // Método para pasar de OrganizationModel a OrganizationEntity
-
-    public OrganizationEntity toEntity () {
-        OrganizationEntity entity = new OrganizationEntity();
-        entity.setId(this.id);
-        entity.setName(this.name);
-        entity.setCIF(this.CIF);
-        entity.setAddress(this.address);
-        entity.setEmail(this.email);
-        entity.setEndSubscription(this.endSubscription);
-        entity.setIsActive(this.isActive);
-        entity.setCreatedAt(this.createdAt);
-        return entity;
-    }
-
-    // Método para pasar de OrganizationCreateDto a OrganizationModel
-
-    public static OrganizationModel fromCreateDto (OrganizationCreateDto createDto) {
-
-        return new OrganizationModel (
-            null,
-            createDto.getName(),
-            createDto.getCIF(),
-            createDto.getAddress(),
-            createDto.getEmail(),
-            createDto.getEndSubscription(),
-            createDto.getIsActive(),
-            createDto.getCreatedAt()
-         );
-    }
-
-    // Método para pasar de un OrganizationUpdateDto a OrganizationModel
-
-    public static OrganizationModel fromUpdateDto (OrganizationUpdateDto updateDto) {
-
-        return new OrganizationModel (
-            updateDto.getId(),
-            updateDto.getName(),
-            updateDto.getCIF(),
-            updateDto.getAddress(),
-            updateDto.getEmail(),
-            updateDto.getEndSubscription(),
-            updateDto.getIsActive(),
-            updateDto.getCreatedAt()
-         );
-    }
-
-    // Método para pasar de OrganizationModel a OrganizationResponseDto
-
-    public OrganizationResponseDto toResponseDto () {
-
-        OrganizationResponseDto responseDto = new OrganizationResponseDto();
-        responseDto.setId(this.id);
-        responseDto.setName(this.name);
-        responseDto.setCIF(this.CIF);
-        responseDto.setAddress(this.address);
-        responseDto.setEmail(this.email);
-        responseDto.setEndSubscription(this.endSubscription);
-        responseDto.setIsActive(this.isActive);
-        responseDto.setCreatedAt(this.createdAt);
-        return responseDto;
-    }
 }
