89e4c9769a353e46632e9bcf2a9c11e7c77282ea
RaiMorales <raimundo.niwe@gmail.com>
2026-06-11 11:35:30 +0200

    Landing sin funcionalidad

--- front/ecotrack-office/src/app/app.routes.ts
+++ front/ecotrack-office/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeLanding } from './landing/components/home-landing/home-landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomeLanding}
+];
