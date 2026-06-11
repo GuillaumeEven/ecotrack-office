9e670327813355bb30b7ff4aac926b473a3aebc3
GuillaumeEven <guillaume@evendev.net>
2026-06-11 17:05:00 +0200

    fix(): merge pr

--- front/ecotrack-office/src/app/app.routes.ts
+++ front/ecotrack-office/src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeLanding } from './landing/components/home-landing/home-landing';
-
-export const routes: Routes = [
-
-    { path: '', component: HomeLanding}
 import { UserProfileComponent } from './features/user-profile/user-profile.component';
 
 export const routes: Routes = [
   { path: 'profile', component: UserProfileComponent },
-  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+  { path: '', component: HomeLanding}
+
 ];
