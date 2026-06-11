a9f22d24bbcd00a6f7b958bd166f407d6b38b07a
joseluis121200 <joseluisrrr9@gmail.com>
2026-06-11 08:29:26 +0200

    Comentario explicando codigo

--- front/ecotrack-office/src/app/interceptors/auth.interceptor.ts
+++ front/ecotrack-office/src/app/interceptors/auth.interceptor.ts
@@ -6,6 +6,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 const CURRENT_USER_ID = 1;
 const CURRENT_USER_ROLE = 'USER';
 
+// Interceptor que añade los headers X-User-Id y X-User-Role a cada petición HTTP.
+// Basicamente clona la petición original y le añade los headers con el id y role del usuario actual 
+// porque no se puede clonar la petición original directamente. 
+// Luego, pasa la petición clonada al siguiente interceptor o al backend.
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const cloned = req.clone({
     setHeaders: {
