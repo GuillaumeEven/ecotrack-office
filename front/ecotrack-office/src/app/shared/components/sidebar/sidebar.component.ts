b0c0b659ae3277684218d9509a130ebf13af2e4b
RaiMorales <raimundo.niwe@gmail.com>
2026-06-18 10:02:31 +0200

    feat(): añado la navegación a la gestion de espacio en el sidebar

--- front/ecotrack-office/src/app/shared/components/sidebar/sidebar.component.ts
+++ front/ecotrack-office/src/app/shared/components/sidebar/sidebar.component.ts
@@ -21,6 +21,7 @@ export class SidebarComponent {
     { icon: 'confirmation_number', label: 'Incidencias', route: '/incidencias' },
     { icon: 'group', label: 'Empleados', route: '/admin/user-management' },
     { icon: 'domain', label: 'Empresa', route: '/organization' },
+    { icon: 'map', label: 'Gestionar espacio', route: '/assets'},
     { icon: 'insights', label: 'Analíticas', route: '/analytics' },
   ];
 
