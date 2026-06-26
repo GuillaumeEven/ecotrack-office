fd12537d44c9daa228a38afb41249d8624d84a77
RaiMorales <raimundo.niwe@gmail.com>
2026-06-26 13:14:43 +0200

    fix(): comienzo a pasar html a español

--- front/ecotrack-office/src/app/app.config.ts
+++ front/ecotrack-office/src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { errorInterceptor } from '@core/interceptors/error.interceptor';
+import localEs from '@angular/common/locales/es';
+
+registerLocaleData(localEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es'}
   ],
 };
