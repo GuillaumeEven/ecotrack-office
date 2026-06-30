ea04ab06d59be23217d4161a7d3e99966a1baecb
GuillaumeEven <guillaume@evendev.net>
2026-06-30 21:47:46 +0200

    test: Add ActiveProfiles annotation for test context in EcotrackOfficeApplicationTests

--- back/ecotrack-office/src/test/java/com/ediae/ecotrack_office/service/EcotrackOfficeApplicationTests.java
+++ back/ecotrack-office/src/test/java/com/ediae/ecotrack_office/service/EcotrackOfficeApplicationTests.java
@@ -2,8 +2,10 @@ package com.ediae.ecotrack_office.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcotrackOfficeApplicationTests {
 
 	@Test
