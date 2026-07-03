3d3726ce7c441bbd6817a7c016b8d07e03fb579b
GuillaumeEven <guillaume@evendev.net>
2026-07-03 12:28:47 +0200

    fix: change DataLoader profile from prod to dev

--- back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/shared/config/DataLoader.java
+++ back/ecotrack-office/src/main/java/com/ediae/ecotrack_office/shared/config/DataLoader.java
@@ -36,7 +36,7 @@ import com.ediae.ecotrack_office.users.enums.Role;
 import com.ediae.ecotrack_office.users.repository.UserRepository;
 
 @Component
-@Profile("prod")
+@Profile("dev")
 public class DataLoader implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
